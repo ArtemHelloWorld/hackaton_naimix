4af4724424726e16a14ea369ee993a681b2b9991
neeearina <arishschkarussia@gmail.com>
2024-11-16 14:18:54 +0500

    add pages with workers list

--- project/workers/urls.py
+++ project/workers/urls.py
@@ -8,5 +8,20 @@ urlpatterns = [
         'questionnaire/',
         workers.views.employee_create,
         name="questionnaire",
-    )
+    ),
+    django.urls.path(
+        'employees/',
+        workers.views.employees,
+        name="employees",
+    ),
+    django.urls.path(
+        'candidates/',
+        workers.views.candidates,
+        name="candidates",
+    ),
+    django.urls.path(
+        'delete_candidate/<int:candidate_id>/',
+        workers.views.delete_candidate,
+        name='delete_candidate'),
+
 ]
--- project/workers/views.py
+++ project/workers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EmployeeForm
+from .models import Employee
 
 
 def employee_create(request):
@@ -7,8 +8,30 @@ def employee_create(request):
         form = EmployeeForm(request.POST)
         if form.is_valid():
             employee = form.save()
-            # TODO: сделать редирект для успешного заполнения формы
-            return redirect('workers', pk=employee.pk)
+            return redirect('workers:employees')
     else:
         form = EmployeeForm()
     return render(request, 'workers/employee_form.html', {'form': form})
+
+
+# Отображение всех работников
+def employees(request):
+    template = 'workers/employees.html'
+    employees = Employee.objects.filter(is_candidate='WR')
+    context = {'employees': employees}
+    return render(request, template, context)
+
+
+# Отображение всех кандидатов
+def candidates(request):
+    template = 'workers/candidates.html'
+    candidates = Employee.objects.filter(is_candidate='CA')
+    context = {'candidates': candidates}
+    return render(request, template, context)
+
+
+# Удаление кандидата
+def delete_candidate(request, candidate_id):
+    candidate = get_object_or_404(Employee, id=candidate_id)
+    candidate.delete()
+    return redirect('workers:candidates')
